842f2baae3984ce887a44b567497f751bf7844ae
Davi Nakano <davinakanoca@gmail.com>
2025-01-15 19:11:42 -0600

    feat: Zod and Fastify integration

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,11 +1,35 @@
 import { env } from '@/env'
 import { fastifyCors } from '@fastify/cors'
 import { fastify } from 'fastify'
+import {
+  hasZodFastifySchemaValidationErrors,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
+import { uploadImageRoute } from './routes/upload-image'
 
 const server = fastify()
 
-server.register(fastifyCors, { origin: '*' })
+server.setValidatorCompiler(validatorCompiler)
+server.setSerializerCompiler(serializerCompiler)
 
-server.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
+server.setErrorHandler((error, request, reply) => {
+  if (hasZodFastifySchemaValidationErrors(error)) {
+    return reply.status(400).send({
+      message: 'Validation error.',
+      issues: error.validation,
+    })
+  }
+
+  // TODO: Add an observability tool
+  console.error(error)
+
+  return reply.status(500).send({ message: 'Internal server error.' })
+})
+
+server.register(fastifyCors, { origin: '*' })
+server.register(uploadImageRoute)
+
+server.listen({ port: env.PORT, host: '0.0.0.0' }).then(() => {
   console.log('HTTP server running!')
 })
